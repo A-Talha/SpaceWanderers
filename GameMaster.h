6a18f3f956b54833123b69736b1b75a478467a75
A-Talha <abdallah.samy.talha@gmail.com>
2024-06-12 02:18:56 +0300

    added basic functions to game master

--- GameMaster.h
+++ GameMaster.h
@@ -8,13 +8,40 @@
 #include "GameObject.h"
 #include "SpaceShip.h"
 
-// Window dimensions
+enum GameState {
+    MAIN_MENU,
+    SURVIVAL_MODE,
+    TIME_MODE,
+    GAME_OVER
+};
 
 class GameMaster {
 public:
     GameMaster();
+    void draw();
+    void updateGameState(GameState newState);
+    void keyUp();
+    void keyDown();
+    void keyLeft();
+    void keyRight();
+    void keySpace();
+    void keyChar(unsigned char key);
+    void GoToMainMenu();
+    void StartSurvivalMode();
+    void StartTimeMode();
+    void EndGameOver();
+
 
 private:
+    GameState state;
+    SpaceShip spaceship;
+    int score;
+    std::vector<GameObject> gameObjects;
+    void DrawMainMenu();
+    void DrawSurvivalMode();
+    void DrawTimeMode();
+    void DrawGameOver();
+
 };
 
 #endif
